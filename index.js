ee97f3657168cee42ef6e142e1d3225711829b4c
NotGayBut5CentsAre5Cents <telerik007@gmail.com>
2018-12-01 17:29:31 +0200

    Added sockets to heatmap

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const express = require('express')
-const app = express()
+
+const express = require('express') 
+const app = require('express')()
 const port = 3000
 const parse = require('csv-parse')
 const fs = require('fs')
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static('public'))
 app.get('/', (req, res) => {
     res.sendFile('index.html');
 })
-app.listen(port,()=>{
-    console.log('App listening on port ' + port)
+io.on('connection', function(client) {  
+    console.log("client connected");
+    client.on('heatmap-change', function(data) {
+        console.log("zdr")
+        io.emit('setData', [data]);
+    })
+});
+http.listen(port, function(){
+    console.log('listening on *:3000');
 });
